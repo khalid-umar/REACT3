2b2941982cf89605e303353facaee0738c505fba
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-08 22:52:32 +0530

    Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,13 @@
+// Navbar.js
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <img src="your-image-url-here" alt="Your Alt Text" role="img" />
+      {/* Rest of your Navbar code */}
+    </nav>
+  );
+}
+
+export default Navbar;
