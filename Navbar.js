0e60649e42723048b2b0128f2eee333297fb82a1
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-08 23:06:02 +0530

    Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -6,8 +6,15 @@ function Navbar() {
     <nav>
       <img src="your-image-url-here" alt="Your Alt Text" role="img" />
       {/* Rest of your Navbar code */}
-    </nav>
-  );
-}
+        <div className="navbar">
+          <h2>NFTium</h2>
+          <ul>
+            <li>Collection</li>
+            <li>Prices</li>
+            <li>Support</li>
+          </ul>
+        </div>
+      );
+    }
 
 export default Navbar;
