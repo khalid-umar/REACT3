1dd29b912fe4e5d8604ba813bcec11ec75c9c4a0
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-08 23:02:58 +0530

    App.js

--- App.js
+++ App.js
@@ -8,16 +8,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Hero />
-      <About />
-      {/* Additional content */}
-    </div>
-  );
-}
+
 
 export default App;
 
